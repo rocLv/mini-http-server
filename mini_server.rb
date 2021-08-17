02b9a2ff011b6173af6d7fcf99c5a9a0ac0b5afb
rocLv <wangqsh999@gmail.com>
2021-08-17 17:05:25 +0800

    respone right content

--- mini_server.rb
+++ mini_server.rb
@@ -3,6 +3,6 @@ require 'socket'
 server = TCPServer.new 5678
 
 while session = server.accept
-  session.puts "Hello world! The time is #{Time.now}"
+  session.puts "HTTP/1.1 200\r\nContent-Type: text/html\r\n\r\n\Hello world! This time is #{Time.now}"
   session.close
 end
